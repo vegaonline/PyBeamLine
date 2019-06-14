fc20cb30d77cc07b40b068ea40198b43d83319ff
Abhijit Bhattacharyya <abhihere@gmail.com>
2019-06-14 17:38:13 +0530

    Editing menus first.

--- pyGUI/mydemirciGUI.py
+++ pyGUI/mydemirciGUI.py
@@ -1,18 +1,62 @@
+""" Python to simulate RFQ as frontend
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import sys
+
+
+from builtins import zip
+from builtins import str
+from builtins import range
+from builtins import object
+
 from tkinter import *
+from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
-###from tkFileDialog import askopenfilename
-root = Tk()
+
+
+class Input(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+
+        self.name_label = Label(root, text = 'Simulator for RFQ')
+        self.name_entry = Entry(root)
+
+    def close_window(self):
+        self.name = self.name_entry.get()
+        self.quit()
+
 
 def NewConfigFile():
-    name="RFQ_New.in"
+    print("Test")
+    
 def OpenFile():
-    name=filedialog.askopenfilename(title="Choose Configuration file", filetypes=(("input file", ".in"),("all files", "*")))
+    # winOpen = Toplevel(root)
+    # menuNewConfigFile = Menu(winOpen)
+    # winOpen.config(menu = menuNewConfigFile)
+    # menuNewConfigFile.add_command(label = "Opening New RFQ Configuration File")
+
+    name = filedialog.askopenfilename(
+        title = "Choose Configuration file", filetypes=(("input file", ".in"),("all files", "*"))
+    )
+    return name
+    
+    
 def About():
     print("GUI for DEMIRCI")
+    
+
+root = Tk()
+root.geometry("700x400+300+300")
 
 
 menu=Menu(root)
 root.config(menu=menu)
+app = Input(root)
 
 filemenu=Menu(menu)
 filemenu.add_command(label="New",  command=NewConfigFile)
@@ -26,6 +70,6 @@ helpmenu.add_command(label="About",command=About)
 menu.add_cascade(label="File", menu=filemenu)
 menu.add_cascade(label="Help", menu=helpmenu)
 
-w= Label(root, text="DEMIRCI")
-w.pack()
+win1= Label(root, text="DEMIRCI")
+win1.pack()
 root.mainloop()
